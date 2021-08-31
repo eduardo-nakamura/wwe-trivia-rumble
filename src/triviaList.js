3b140d92bac58d547771144c370cef9fee8f97a6
eduardo-nakamura <edu-nakamura@hotmail.com>
2021-08-31 15:11:49 -0300

    teste tabela

--- src/triviaList.js
+++ src/triviaList.js
@@ -1,20 +1,47 @@
 import React, { useState, useEffect } from "react";
-import Grid from "@material-ui/core/Grid";
 
 import TriviaListJson from "./answers.json";
-import Button from "@material-ui/core/Button";
+import MenuItem from "@material-ui/core/MenuItem";
+// table
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Paper from "@material-ui/core/Paper";
+
+// table
+import Select from "@material-ui/core/Select";
 import Container from "@material-ui/core/Container";
 import Input from "@material-ui/core/Input";
-import InputLabel from '@material-ui/core/InputLabel';
-import InputAdornment from '@material-ui/core/InputAdornment';
-import ClearIcon from '@material-ui/icons/Clear';
-import TextField from "@material-ui/core/TextField";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import ClearIcon from "@material-ui/icons/Clear";
 import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
 import CardContent from "@material-ui/core/CardContent";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
-import TitleImg from "./img/logo.png";
+// columnsStart
+
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
+  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
+  createData("Eclair", 262, 16.0, 24, 6.0),
+  createData("Cupcake", 305, 3.7, 67, 4.3),
+  createData("Gingerbread", 356, 16.0, 49, 3.9),
+];
+
+// columnsEnd
 const useStyles = makeStyles({
   root: {
     minWidth: 275,
@@ -32,12 +59,11 @@ const useStyles = makeStyles({
     transform: "scale(0.8)",
   },
   input: {
-    backgroundColor: "rgba(255,255,255,0.3)",
+    backgroundColor: "rgba(255,255,255,0.1)",
     color: "#a6acdc",
     margin: "20px 0px 0px 0px",
     padding: "15px",
     width: "100%",
-    borderRadius: "10px",
     outline: "none",
     border: 0,
   },
@@ -62,6 +88,17 @@ const useStyles = makeStyles({
     color: "#e2e4f5",
     padding: "10px",
   },
+  table: {
+    backgroundColor: "rgba(255,255,255,0.1)",
+    borderRadius: '7px'
+  },
+  tableHead: {
+    color: "#e2e4f5",
+    padding: "10px 0px 10px 0px",
+  },
+  tableBody: {
+    color: "#e2e4f5",
+  },
 });
 
 function TriviaList() {
@@ -78,65 +115,58 @@ function TriviaList() {
     866, 867, 868, 869, 870, 871, 872, 873, 874, 875, 876, 877, 878, 879, 880,
     881, 882, 883, 884, 885, 886, 97, 98,
   ];
+  const [maxNum, setMaxNum] = useState(10);
   const [triviaObj, setTriviaObj] = useState(TriviaListJson["QuestionList"]);
   const [searchTerm, setSearchTerm] = useState("");
   const [searchTermLength, setSearchTermLength] = useState(0);
+  const [questions, setQuestions] = useState([]);
   const [filteredQuestions, setFilteredQuestions] = useState([]);
   const classes = useStyles();
-  // useEffect(() => {
-  //   if (searchTerm.length > 0) {
-  //     const results = triviaObj.filter((man) =>
-  //       man.Question.toLowerCase().includes(searchTerm.toLowerCase())
-  //     );
-  //     let debouncer = setTimeout(() => {
-  //       setSearchTermLength(results.length);
-  //       setFilteredQuestions(results.slice(0, 20));
-  //     }, 2000);
-  //   }
-  // }, [searchTerm]);
+
   useEffect(() => {
-    // let debouncer = setTimeout(() => {
-    //   const results = triviaObj.filter((man) =>
-    //     man.Question.toLowerCase().includes(searchTerm.toLowerCase())
-    //   );
-    //   setSearchTermLength(results.length);
-    //   setFilteredQuestions(results.slice(0, 20));
-    // }, 500);
     const results = triviaObj.filter((man) =>
       man.Question.toLowerCase().includes(searchTerm.toLowerCase())
     );
     setSearchTermLength(results.length);
-    setFilteredQuestions(results.slice(0, 30));
+    setQuestions(results);
+    setFilteredQuestions(results.slice(0, maxNum));
   }, [searchTerm]);
+
   const clearSearch = () => {
-    setSearchTerm('');
-  }
+    setSearchTerm("");
+  };
+
+  const maxNumSelect = (event) => {
+    setMaxNum(event.target.value);
+    setFilteredQuestions(questions.slice(0, event.target.value));
+  };
+
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
-  };
-  const nextPokemons = (id) => {
-    return `./img/${id}.png`;
   };
 
   return (
     <Container>
-      <h1 className={classes.titlePage}>
-        {/* <img src={TitleImg} alt="WWE CHAMPIONS" /> */}
-        WWE TRIVIA RUMBLE
-      </h1>
+      <h1 className={classes.titlePage}>WWE TRIVIA RUMBLE</h1>
       <form noValidate autoComplete="off">
-        {/* <label className={classes.label}>Type Question Here</label>
-        <input
-          className={classes.input}
-          id="standard-basic"
-          label="Type Question Here"
-          value={searchTerm}
-          onChange={handleChange}
-        />
-        <Button variant="outlined">X</Button> */}
-        <InputLabel className={classes.label} htmlFor="input-with-icon-adornment">
+        <InputLabel
+          className={classes.label}
+          htmlFor="input-with-icon-adornment"
+        >
           Type Question Here
         </InputLabel>
+        {/* <button onClick={changeMaxNum20}>20</button> */}
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          value={maxNum}
+          onChange={maxNumSelect}
+        >
+          <MenuItem value={10}>Ten</MenuItem>
+          <MenuItem value={20}>Twenty</MenuItem>
+          <MenuItem value={30}>Thirty</MenuItem>
+        </Select>
+        {/* <button onClick={changeMaxNum30}>30</button> */}
         <Input
           className={classes.input}
           id="standard-basic"
@@ -144,8 +174,8 @@ function TriviaList() {
           value={searchTerm}
           onChange={handleChange}
           startAdornment={
-            <InputAdornment position="start" >
-              <ClearIcon onClick={clearSearch}/>
+            <InputAdornment position="start">
+              <ClearIcon onClick={clearSearch} />
             </InputAdornment>
           }
         />
@@ -160,22 +190,64 @@ function TriviaList() {
         </p>
       )}
       <div className="pokemon-list">
-        <Grid container spacing={3}>
+        <TableContainer >
+          <Table
+            className={classes.table}
+            size="small"
+            aria-label="a dense table"
+          >
+            <TableHead>
+              <TableRow  style={{ borderBottom: '3px #e2e4f5 solid' }}>
+                <TableCell><h3 className={classes.tableHead}>Question</h3> </TableCell>
+                <TableCell align="left"><h3 className={classes.tableHead}>Answer</h3></TableCell>
+                <TableCell align="left"><h3 className={classes.tableHead}>Image</h3></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {filteredQuestions.map((row) => (
+                <TableRow key={row.Id}  style={{ borderBottom: '3px #e2e4f5 solid' }}>
+                  <TableCell style={{ border: 0 }} component="th" scope="row">
+                    <p className={classes.tableBody}>{row.Question}</p>                    
+                  </TableCell>
+                  <TableCell style={{ border: 0 }} align="left">            
+                    <p style={{ fontSize: '18px' }} className={classes.tableBody}>{row.Answer}</p>
+                  </TableCell>
+                  <TableCell style={{ border: 0 }} align="left">
+                    <div
+                      key={"img-" + row.Id}
+                      style={{
+                        height: "30px",
+                        width: "30px",
+                        backgroundImage: `url(./img/${row.Id}.png)`,
+                      }}
+                    ></div>
+                  </TableCell>
+                  {/* <TableCell align="right">{row.fat}</TableCell>
+                  <TableCell align="right">{row.carbs}</TableCell>
+                  <TableCell align="right">{row.protein}</TableCell> */}
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+        {/* <Grid container spacing={3}>
           {filteredQuestions.map((questionn) => (
-            <Grid item xs={12} sm={6} md={4} lg={3}>
-              {/* <h4>{questionn.Question}</h4>
-              <div
-                style={{
-                  height: "100px",
-                  width: "100px",
-                  backgroundImage: `url(./img/${questionn.Id}.png)`,
-                }}
-              ></div>
-              <p>{questionn.Answer}</p> */}
-
-              <Card className={classes.root} variant="outlined">
-                <CardContent>
+            <Grid
+              key={"grid-" + questionn.Id}
+              item
+              xs={12}
+              sm={12}
+              md={12}
+              lg={12}
+            >
+              <Card
+                key={"card-" + questionn.Id}
+                className={classes.root}
+                variant="outlined"
+              >
+                <CardContent key={"card-content-" + questionn.Id}>
                   <Typography
+                    key={"title-" + questionn.Id}
                     className={classes.title}
                     color="textSecondary"
                     gutterBottom
@@ -183,6 +255,7 @@ function TriviaList() {
                     Q: {questionn.Question}
                   </Typography>
                   <div
+                    key={"img-" + questionn.Id}
                     style={{
                       height: imgNums.find((idd) => idd == questionn.Id)
                         ? "100px"
@@ -193,6 +266,7 @@ function TriviaList() {
                   ></div>
 
                   <Typography
+                    key={"answer-" + questionn.Id}
                     variant="body2"
                     className={classes.answer}
                     component="p"
@@ -200,13 +274,11 @@ function TriviaList() {
                     A: {questionn.Answer}
                   </Typography>
                 </CardContent>
-                {/* <CardActions>
-                  <Button size="small">Learn More</Button>
-                </CardActions> */}
+                
               </Card>
             </Grid>
           ))}
-        </Grid>
+        </Grid> */}
       </div>
     </Container>
   );
